f13f11898c10b0a7d051b6ba61a1d2de8afe2929
Vitaliy <theyamshikov@yandex.ru>
2021-10-21 10:06:36 +0600

    upd

--- src/Domain/config/container-new.php
+++ src/Domain/config/container-new.php
@@ -17,5 +17,6 @@ return [
     'entities' => [
         'ZnBundle\\Language\\Domain\\Entities\\BundleEntity' => 'ZnBundle\\Language\\Domain\\Interfaces\\Repositories\\BundleRepositoryInterface',
         'ZnBundle\\Language\\Domain\\Entities\\TranslateEntity' => 'ZnBundle\\Language\\Domain\\Interfaces\\Repositories\\TranslateRepositoryInterface',
+        'ZnBundle\\Language\\Domain\\Entities\\LanguageEntity' => 'ZnBundle\\Language\\Domain\\Interfaces\\Repositories\\LanguageRepositoryInterface',
     ],
 ];
