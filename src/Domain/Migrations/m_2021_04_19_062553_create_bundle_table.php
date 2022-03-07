7bbd25aa8598cbb579fa4f73befbd1d8626901ea
Vitaliy <theyamshikov@yandex.ru>
2022-03-07 19:48:42 +0600

    upd

--- src/Domain/Migrations/m_2021_04_19_062553_create_bundle_table.php
+++ src/Domain/Migrations/m_2021_04_19_062553_create_bundle_table.php
@@ -3,7 +3,7 @@
 namespace Migrations;
 
 use Illuminate\Database\Schema\Blueprint;
-use ZnLib\Migration\Domain\Base\BaseCreateTableMigration;
+use ZnDatabase\Migration\Domain\Base\BaseCreateTableMigration;
 
 class m_2021_04_19_062553_create_bundle_table extends BaseCreateTableMigration
 {
