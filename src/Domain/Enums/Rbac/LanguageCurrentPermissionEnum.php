d1c7c779f1d7d89234290120c6964936375ea2e4
Vitaliy <theyamshikov@yandex.ru>
2022-05-02 21:01:17 +0600

    upd

--- src/Domain/Enums/Rbac/LanguageCurrentPermissionEnum.php
+++ src/Domain/Enums/Rbac/LanguageCurrentPermissionEnum.php
@@ -2,7 +2,7 @@
 
 namespace ZnBundle\Language\Domain\Enums\Rbac;
 
-use ZnCore\Base\Interfaces\GetLabelsInterface;
+use ZnCore\Contract\Enum\Interfaces\GetLabelsInterface;
 
 class LanguageCurrentPermissionEnum implements GetLabelsInterface
 {
