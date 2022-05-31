0661637fa5b1470d3f685f7fb8ea54d54a95db6b
Vitaliy <theyamshikov@yandex.ru>
2022-05-31 11:27:18 +0600

    upd

--- src/BundleNew.php
+++ src/BundleNew.php
@@ -2,61 +2,11 @@
 
 namespace ZnBundle\Language;
 
-use ZnCore\Base\Libs\App\Base\BaseBundle;
+use ZnCore\Base\Helpers\DeprecateHelper;
 
-class BundleNew extends BaseBundle
+//DeprecateHelper::hardThrow();
+
+class BundleNew extends Bundle
 {
 
-    public function symfonyWeb(): array
-    {
-        return [
-            __DIR__ . '/Symfony4/Web/config/routing-switch.php',
-        ];
-    }
-
-    public function symfonyAdmin(): array
-    {
-        return [
-            __DIR__ . '/Symfony4/Web/config/routing-switch.php',
-        ];
-    }
-
-    public function i18next(): array
-    {
-        return [
-            'language' => 'vendor/znbundle/language/src/Domain/i18next/__lng__/__ns__.json',
-        ];
-    }
-
-    public function yiiWeb(): array
-    {
-        return [
-            'modules' => [
-                'language' => __NAMESPACE__ . '\Yii2\Web\Module',
-            ],
-        ];
-    }
-
-    public function yiiAdmin(): array
-    {
-        return [
-            'modules' => [
-                'language' => __NAMESPACE__ . '\Yii2\Admin\Module',
-            ],
-        ];
-    }
-
-    public function migration(): array
-    {
-        return [
-            '/vendor/znbundle/language/src/Domain/Migrations',
-        ];
-    }
-
-    public function container(): array
-    {
-        return [
-            __DIR__ . '/Domain/config/container-new.php',
-        ];
-    }
 }
